2b9242f05426de7eeacde6d20879f2694ef5c850
Navneetkumar20 <nksingh2401gmail.com>
2025-08-19 11:38:28 +0530

    cart code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
   const { isSeller, router } = useAppContext();
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+    <nav className="flex items-center justify-between px-4 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
       <Image
-        className="cursor-pointer w-28 md:w-32"
+        className="cursor-pointer w-16 h-14 md:w-32"
         onClick={() => router.push('/')}
-        src={assets.logo}
+        src={assets.cart_icon}
         alt="logo"
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
